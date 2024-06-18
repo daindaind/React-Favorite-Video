b8ccec1d39e5ff00c67cadaa8c398abf4d4fc70d
daindaind <cdin23@naver.com>
2024-06-18 23:44:22 +0900

    Feat: MovieDetailPage 구현

--- src/pages/movies/detail/MovieDetailPage.jsx
+++ src/pages/movies/detail/MovieDetailPage.jsx
@@ -1,5 +1,25 @@
+import { useLocation, useParams } from "react-router-dom";
+import styles from "./MovieDetailPage.module.css";
+
 const MovieDetailPage = () => {
-  return <div>MovieDetailPage</div>;
+  const { id } = useParams();
+  const { state } = useLocation();
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.InfoLayout}>
+        <img src={state.image} className={styles.Img} />
+        <div className={styles.Content}>
+          <p className={styles.Title}>{state.title}</p>
+          <p className={styles.Contents}>{state.creator}</p>
+          <p className={styles.Contents}>{state.summary}</p>
+        </div>
+      </div>
+      <div className={styles.Instructions}>
+        <p className={styles.InstructionsText}>{state.instructions}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetailPage;
