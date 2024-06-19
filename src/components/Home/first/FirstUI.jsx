4282e56270e4658ce20a4914c59089f87b69015d
daindaind <cdin23@naver.com>
2024-06-19 17:15:04 +0900

    Feat: Home SecondUI 구현

--- src/components/Home/first/FirstUI.jsx
+++ src/components/Home/first/FirstUI.jsx
@@ -8,17 +8,20 @@ const FirstUI = () => {
       <h1 className={styles.Title}>
         모두의 <span>인생작</span> 전부 여기에서.
       </h1>
-      <h3 className={styles.SubTitle}>나만의 모든 Movie, Drama, Animation</h3>
-      <div className={styles.MovieCardContainer} />
-      <div className={`${styles.Card} ${styles.Card1}`}>
-        <MoviePoster movie={dummyMovies[0]} />
+      <h3 className={styles.SubTitle}>나만의 모든 영화, 드라마, 애니메이션</h3>
+
+      <div className={styles.PosterContainer}>
+        <div className={`${styles.Card} ${styles.Card1}`}>
+          <MoviePoster movie={dummyMovies[0]} />
+        </div>
+        <div className={`${styles.Card} ${styles.Card2}`}>
+          <MoviePoster movie={dummyMovies[6]} />
+        </div>
+        <div className={`${styles.Card} ${styles.Card3}`}>
+          <MoviePoster movie={dummyMovies[2]} />
+        </div>
       </div>
-      <div className={`${styles.Card} ${styles.Card2}`}>
-        <MoviePoster movie={dummyMovies[6]} />
-      </div>
-      <div className={`${styles.Card} ${styles.Card3}`}>
-        <MoviePoster movie={dummyMovies[2]} />
-      </div>
+      <div className={styles.MovieCardBg} />
     </div>
   );
 };
--- src/components/Home/second/SecondUI.jsx
+++ src/components/Home/second/SecondUI.jsx
@@ -1,8 +1,33 @@
+import dummyMovies from "../../../data/dummyMovies";
+import MoviePoster from "../../Moives/moviePoster/MoviePoster";
+import styles from "./SecondUI.module.css";
+
 const SecondUI = () => {
   return (
-    <div>
-      <h1>나만 알고 싶은 작품을 소개하기.</h1>
-      <h3>Share your favorite work</h3>
+    <div className={styles.Container}>
+      <h1 className={styles.Title}>나만 알고 싶은 작품을 소개하기.</h1>
+      <h3 className={styles.SubTitle}>
+        내가 좋아하는 영화/드라마를 다른 사람과 공유해보세요
+      </h3>
+      <div className={styles.Poster}>
+        <MoviePoster movie={dummyMovies[4]} />
+
+        <div className={styles.BubblePosition1}>
+          <div className={styles.SpeechBubbleLeft}>
+            <p className={styles.BubbleText}>
+              이 영화에 대해 다른 사람과{"\n"}얘기하고 싶어
+            </p>
+          </div>
+        </div>
+        <div className={styles.BubblePosition2}>
+          <div className={styles.SpeechBubbleRight}>
+            <p className={styles.BubbleText}>내 취향을 공유하고 싶은데..</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${styles.Circle} ${styles.Circle1}`}></div>
+      <div className={`${styles.Circle} ${styles.Circle2}`}></div>
     </div>
   );
 };
