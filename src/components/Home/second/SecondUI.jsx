71c2ec95ad94a72feaf8db3860be362dd7144cf4
daindaind <cdin23@naver.com>
2024-06-20 01:34:53 +0900

    Style: Home SecondUI styles 수정

--- src/components/Home/second/SecondUI.jsx
+++ src/components/Home/second/SecondUI.jsx
@@ -9,49 +9,31 @@ const SecondUI = () => {
   const isInViewport = useIntersectionObsever(ref);
   return (
     <div className={styles.Container} ref={ref}>
-      <h1 className={isInViewport && `${styles.Title} ${styles.slideUp}`}>
+      <h1 className={isInViewport && styles.Title}>
         나만 알고 싶은 작품을 소개하기.
       </h1>
-      <h3 className={isInViewport && `${styles.SubTitle} ${styles.slideUp}`}>
+      <h3 className={isInViewport && styles.SubTitle}>
         내가 좋아하는 영화/드라마를 다른 사람과 공유해보세요
       </h3>
-      <div className={isInViewport && `${styles.Poster} ${styles.slideUp}`}>
+      <div className={isInViewport && styles.Poster}>
         <MoviePoster movie={dummyMovies[4]} />
 
-        <div
-          className={
-            isInViewport && `${styles.BubblePosition1} ${styles.slideUp}`
-          }
-        >
+        <div className={isInViewport && styles.BubblePosition1}>
           <div className={styles.SpeechBubbleLeft}>
             <p className={styles.BubbleText}>
-              이 영화에 대해 다른 사람과{"\n"}얘기하고 싶어 : {"("}
+              이 영화에 대해 다른 사람과{"\n"}얘기하고 싶어 :{"("}
             </p>
           </div>
         </div>
-        <div
-          className={
-            isInViewport && `${styles.BubblePosition2} ${styles.slideUp}`
-          }
-        >
+        <div className={isInViewport && styles.BubblePosition2}>
           <div className={styles.SpeechBubbleRight}>
             <p className={styles.BubbleText}>내 취향을 공유하고 싶은데..</p>
           </div>
         </div>
       </div>
 
-      <div
-        className={
-          isInViewport &&
-          `${styles.Circle} ${styles.Circle1} ${styles.moveRightToLeft}`
-        }
-      />
-      <div
-        className={
-          isInViewport &&
-          `${styles.Circle} ${styles.Circle2} ${styles.moveLeftToRight}`
-        }
-      />
+      <div className={isInViewport && `${styles.Circle} ${styles.Circle1}`} />
+      <div className={`${styles.Circle} ${styles.Circle2}`} />
     </div>
   );
 };
